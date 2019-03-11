ddafbf528f61d040b0c57eb5352f639e0b714d6a
Quentin LATOUR <Quentour69100@gmail.com>
2019-03-11 14:30:50 +0100

    indiana

--- indiana.php
+++ indiana.php
@@ -1,14 +1,10 @@
 <?php
-$nomFilm = 'Indiana Jones';
-$vu = true;
-$dateSorti = '1989';
-$noteFilm = '8.5/10';
- echo "$nomFilm";
- if ($vu == true) {
- echo "<br>vu";
-} else {
-echo "<br> pas vu";
-}
- echo "<br>$dateSorti";
- echo "<br>$noteFilm";
-?>	
+$name = "Indiana Jones and the Last Crusade";
+$seen=true;
+$year=1989;
+$rating=8.2;
+echo $name . "<br>";
+echo $seen . "<br>";
+echo $year . "<br>";
+echo $rating . "<br>";
+?>
